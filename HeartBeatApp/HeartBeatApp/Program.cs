59e42416da180441409ad591f22a888cbef94355
Mehmet Yalçin <88285905+Y-Mehmet@users.noreply.github.com>
2025-05-08 00:38:19 +0300

    first request added

---
+++ HeartBeatApp/HeartBeatApp/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using HeartBeatApp;
+using System.Text.Json;
+
+class Program
+{
+    static  async Task Main(string[] args)
+    {
+        var json = File.ReadAllText("C:\\Github\\AdisyoCase\\HeartBeatApp\\HeartBeatApp\\config.json");
+        var config= JsonSerializer.Deserialize<AppConfig>(json);
+        List<Task> urlMonitorTasks = new List<Task>();
+        foreach (var urlData in config.URLDatas)
+        {
+           var urlMonitor= new UrlMonitor(urlData);
+            Console.WriteLine("Running Program main method");
+            urlMonitorTasks.Add(urlMonitor.StartTask());
+
+        }
+        await Task.WhenAll(urlMonitorTasks);
+    }
+}
+public class EmailConfig
+{
+    public required string SmtpServer { get; set; }
+    public int Port { get; set; }
+    public bool EnableSsl { get; set; }
+    public required string From { get; set; }
+    public string To { get; set; }
+    public required string UserName { get; set; }
+    public required string Password { get; set; }
+}
+public class URLDataConfig
+{
+    public required string URL { get; set; }
+    public int Delay { get; set; }
+    public required EmailConfig Email { get; set; }
+}
+public class AppConfig
+{
+   public required URLDataConfig[] URLDatas { get; set; }
+
+
+}
---
+++ HeartBeatApp/HeartBeatApp/UrlMonitor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HeartBeatApp
+{
+    internal class UrlMonitor
+    {
+        private readonly URLDataConfig _urlDataConfig;
+        private string _baseLogPath = "C:\\Github\\AdisyoCase\\HeartBeatApp\\HeartBeatApp\\log.txt";
+        public UrlMonitor(URLDataConfig urlDataCanfig)
+        {
+            _urlDataConfig = urlDataCanfig;
+        }
+        public async Task StartTask()
+        {
+            Console.WriteLine(" starting task ");
+            try
+            {
+                HttpClient client = new HttpClient();
+                using HttpResponseMessage response = await client.GetAsync(_urlDataConfig.URL);
+                if (response.IsSuccessStatusCode)
+                {
+                    int statusCode = (int)response.StatusCode;
+                    string msg = "" + response.ReasonPhrase;
+
+                    await AddLog(_baseLogPath, statusCode, msg, _urlDataConfig.URL);
+                }
+                else
+                {
+                    int statusCode = (int)response.StatusCode;
+                    string msg = "" + response.ReasonPhrase;
+
+                    await AddLog(_baseLogPath, statusCode, msg, _urlDataConfig.URL);
+                }
+            }catch( HttpRequestException e)
+            {
+                await AddLog(_baseLogPath,000,e.Message,_urlDataConfig.URL);
+            }
+        }
+        private async Task AddLog(string path, int statusCode, string msg, string URL)
+        {
+            StreamWriter streamWriter = new StreamWriter(path, true);
+            string log = $"[{DateTime.Now}] [{statusCode}]  [{msg}] [{URL}]";
+            Console.WriteLine(log);
+            await streamWriter.WriteLineAsync(log);
+            streamWriter.Close();
+
+        }
+
+    }
+}
